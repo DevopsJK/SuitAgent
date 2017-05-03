84f3edee3682c22a0ab73516edd7514a3eacf16c
QianLong <642321251@qq.com>
2017-05-03 09:44:25 +0800

    修复JMX采集出现的不可用的counter
    Signed-off-by: QianLong <642321251@qq.com>

--- src/main/java/com/yiji/falcon/agent/falcon/FalconReportObject.java
+++ src/main/java/com/yiji/falcon/agent/falcon/FalconReportObject.java
@@ -5,7 +5,9 @@
 package com.yiji.falcon.agent.falcon;
 
 import com.yiji.falcon.agent.util.StringUtils;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.management.ObjectName;
 
@@ -18,34 +20,46 @@ import javax.management.ObjectName;
  * push到falcon的数据报告对象
  * @author guqiu@yiji.com
  */
-@Data
+@ToString
 public class FalconReportObject implements Cloneable{
 
     /**
      *  标明Metric的主体(属主)，比如metric是cpu_idle，那么Endpoint就表示这是哪台机器的cpu_idle
      */
+    @Getter
+    @Setter
     private String endpoint;
     /**
      * 最核心的字段，代表这个采集项具体度量的是什么, 比如是cpu_idle呢，还是memory_free, 还是qps
      */
+    @Getter
+    @Setter
     private String metric;
     /**
      * 表示汇报该数据时的unix时间戳，注意是整数，代表的是秒
      */
+    @Getter
+    @Setter
     private long timestamp;
     /**
      * 表示该数据采集项的汇报周期，这对于后续的配置监控策略很重要，必须明确指定。
      */
+    @Getter
+    @Setter
     private int step;
     /**
      * 代表该metric在当前时间点的值,数值类型
      */
+    @Getter
+    @Setter
     private String value;
     /**
      * 只能是COUNTER或者GAUGE二选一，前者表示该数据采集项为计时器类型，后者表示其为原值 (注意大小写)
      GAUGE：即用户上传什么样的值，就原封不动的存储
      COUNTER：指标在存储和展现的时候，会被计算为speed，即（当前值 - 上次值）/ 时间间隔
      */
+    @Getter
+    @Setter
     private CounterType counterType;
     /**
      * 一组逗号分割的键值对, 对metric进一步描述和细化, 可以是空字符串. 比如idc=lg，比如service=xbox等，多个tag之间用逗号分割
@@ -55,6 +69,8 @@ public class FalconReportObject implements Cloneable{
     /**
      * 仅供系统使用
      */
+    @Getter
+    @Setter
     private ObjectName objectName;
 
     @Override
@@ -82,4 +98,11 @@ public class FalconReportObject implements Cloneable{
         return this;
     }
 
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags.replace("\"","");
+    }
 }
