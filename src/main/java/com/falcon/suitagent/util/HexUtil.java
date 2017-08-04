40efb97664a3755b838e1da18e4b3face67f6f64
QianLong <642321251@qq.com>
2017-08-04 14:18:55 +0800

    添加HexUtil
    Signed-off-by: QianLong <642321251@qq.com>

---
+++ src/main/java/com/falcon/suitagent/util/HexUtil.java
@@ -0,0 +1,114 @@
+/*
+ * www.msxf.com Inc.
+ * Copyright (c) 2017 All Rights Reserved
+ */
+package com.falcon.suitagent.util;
+//             ,%%%%%%%%,
+//           ,%%/\%%%%/\%%
+//          ,%%%\c "" J/%%%
+// %.       %%%%/ o  o \%%%
+// `%%.     %%%%    _  |%%%
+//  `%%     `%%%%(__Y__)%%'
+//  //       ;%%%%`\-/%%%'
+// ((       /  `%%%%%%%'
+//  \\    .'          |
+//   \\  /       \  | |
+//    \\/攻城狮保佑) | |
+//     \         /_ | |__
+//     (___________)))))))                   `\/'
+/*
+ * 修订记录:
+ * long.qian@msxf.com 2017-08-04 14:08 创建
+ */
+
+/**
+ * @author long.qian@msxf.com
+ */
+public class HexUtil {
+
+    private static boolean isCnSymbol(char ch) {
+        if (0x3004 <= ch && ch <= 0x301C) return true;
+        if (0x3020 <= ch && ch <= 0x303F) return true;
+        return false;
+    }
+    private static boolean isEnSymbol(char ch){
+
+        if (ch == 0x40) return true;
+        if (ch == 0x2D || ch == 0x2F) return true;
+        if (0x23 <= ch && ch <= 0x26) return true;
+        if (0x28 <= ch && ch <= 0x2B) return true;
+        if (0x3C <= ch && ch <= 0x3E) return true;
+        if (0x5B <= ch && ch <= 0x60) return true;
+        if (0x7B <= ch && ch <= 0x7E) return true;
+
+        return false;
+    }
+
+    private static boolean isPunctuation(char ch){
+        if(isCjkPunc(ch)) return true;
+        if(isEnPunc(ch)) return true;
+
+        if(0x2018 <= ch && ch <= 0x201F) return true;
+        if(ch == 0xFF01 || ch == 0xFF02) return true;
+        if(ch == 0xFF07 || ch == 0xFF0C) return true;
+        if(ch == 0xFF1A || ch == 0xFF1B) return true;
+        if(ch == 0xFF1F || ch == 0xFF61) return true;
+        if(ch == 0xFF0E) return true;
+        if(ch == 0xFF65) return true;
+
+        return false;
+    }
+    private static boolean isEnPunc(char ch){
+        if (0x21 <= ch && ch <= 0x22) return true;
+        if (ch == 0x27 || ch == 0x2C) return true;
+        if (ch == 0x2E || ch == 0x3A) return true;
+        if (ch == 0x3B || ch == 0x3F) return true;
+
+        return false;
+    }
+    private static boolean isCjkPunc(char ch){
+        if (0x3001 <= ch && ch <= 0x3003) return true;
+        if (0x301D <= ch && ch <= 0x301F) return true;
+
+        return false;
+    }
+
+    private static boolean isSymbol(char ch)
+    {
+        if(isCnSymbol(ch)) return true;
+        if(isEnSymbol(ch))return true;
+
+        if(0x2010 <= ch && ch <= 0x2017) return true;
+        if(0x2020 <= ch && ch <= 0x2027) return true;
+        if(0x2B00 <= ch && ch <= 0x2BFF) return true;
+        if(0xFF03 <= ch && ch <= 0xFF06) return true;
+        if(0xFF08 <= ch && ch <= 0xFF0B) return true;
+        if(ch == 0xFF0D || ch == 0xFF0F) return true;
+        if(0xFF1C <= ch && ch <= 0xFF1E) return true;
+        if(ch == 0xFF20 || ch == 0xFF65) return true;
+        if(0xFF3B <= ch && ch <= 0xFF40) return true;
+        if(0xFF5B <= ch && ch <= 0xFF60) return true;
+        if(ch == 0xFF62 || ch == 0xFF63) return true;
+        if(ch == 0x0020 || ch == 0x3000) return true;
+        return false;
+
+    }
+
+    /**
+     * 过滤字符串，返回可见字符形式（数字、字母、符号等）
+     * @param str
+     * @return
+     */
+    public static String filter(String str){
+        StringBuilder sb = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            if (String.valueOf(c).matches("([a-z]|[A-Z]|\\d|\\s|[\\u4e00-\\u9fa5])+") ||
+                    isSymbol(c) ||
+                    isPunctuation(c)){
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+}
