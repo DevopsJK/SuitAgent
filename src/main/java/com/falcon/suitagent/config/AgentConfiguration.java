7f8ad5c1b7b9e6b25d2ce57a344ced5c396fd11a
QianLong <642321251@qq.com>
2017-12-13 15:40:35 +0800

    hostUtil优化
    Signed-off-by: QianLong <642321251@qq.com>

--- src/main/java/com/falcon/suitagent/config/AgentConfiguration.java
+++ src/main/java/com/falcon/suitagent/config/AgentConfiguration.java
@@ -29,7 +29,7 @@ public enum  AgentConfiguration {
     INSTANCE;
 
     //版本不能大于 x.9
-    public static final float VERSION = (float)  18.4;
+    public static final float VERSION = (float)  18.5;
 
     /**
      * quartz配置文件路径
--- src/main/java/com/falcon/suitagent/util/HostUtil.java
+++ src/main/java/com/falcon/suitagent/util/HostUtil.java
@@ -23,7 +23,9 @@ package com.falcon.suitagent.util;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.net.*;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -37,34 +39,37 @@ public class HostUtil {
     /**
      * 获取本机主机名
      * @return
-     * @throws UnknownHostException
      */
-    public static String getHostName() throws UnknownHostException, SocketException {
+    public static String getHostName() {
         String osName = System.getProperty("os.name").toLowerCase();
         String hostName = "localhost";
-        Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
-        InetAddress ip;
-        while (allNetInterfaces.hasMoreElements())
-        {
-            NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
-            Enumeration addresses = netInterface.getInetAddresses();
-            while (addresses.hasMoreElements())
+        try {
+            Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            InetAddress ip;
+            while (allNetInterfaces.hasMoreElements())
             {
-                ip = (InetAddress) addresses.nextElement();
-                if (ip != null && ip instanceof Inet4Address)
+                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+                Enumeration addresses = netInterface.getInetAddresses();
+                while (addresses.hasMoreElements())
                 {
-                    if (!ip.getHostName().startsWith("localhost") && !ip.getHostName().startsWith("1") && !ip.getHostName().startsWith("2")){
-                        hostName = ip.getHostName();
+                    ip = (InetAddress) addresses.nextElement();
+                    if (ip != null && ip instanceof Inet4Address)
+                    {
+                        if (!ip.getHostName().startsWith("localhost") && !ip.getHostName().startsWith("1") && !ip.getHostName().startsWith("2")){
+                            hostName = ip.getHostName();
+                        }
                     }
                 }
             }
-        }
-        if (hostName.equals("localhost")){
-            InetAddress addr = InetAddress.getLocalHost();
-            hostName = addr.getHostName();
-        }
-        if ("localhost".equals(hostName)){
-            log.warn("本机的主机名获取可能失败（返回默认的localhost），当前系统环境：{}",osName);
+            if ("localhost".equals(hostName)){
+                InetAddress addr = InetAddress.getLocalHost();
+                hostName = addr.getHostName();
+            }
+            if ("localhost".equals(hostName)){
+                log.warn("本机的主机名获取可能失败（返回默认的localhost），当前系统环境：{}",osName);
+            }
+        } catch (Exception e) {
+           log.error("",e);
         }
 
         return hostName;
@@ -73,34 +78,37 @@ public class HostUtil {
     /**
      * 获取本机IP地址
      * @return
-     * @throws UnknownHostException
      */
-    public static String getHostIp() throws UnknownHostException, SocketException {
+    public static String getHostIp() {
         String ipAddress = "127.0.0.1";
         String osName = System.getProperty("os.name").toLowerCase();
-        Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
-        InetAddress ip;
-        while (allNetInterfaces.hasMoreElements())
-        {
-            NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
-            Enumeration addresses = netInterface.getInetAddresses();
-            while (addresses.hasMoreElements())
+        try {
+            Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            InetAddress ip;
+            while (allNetInterfaces.hasMoreElements())
             {
-                ip = (InetAddress) addresses.nextElement();
-                if (ip != null && ip instanceof Inet4Address)
+                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+                Enumeration addresses = netInterface.getInetAddresses();
+                while (addresses.hasMoreElements())
                 {
-                    if (!ip.getHostAddress().startsWith("127.0.0.1") && !ip.getHostAddress().startsWith("localhost")){
-                        ipAddress = ip.getHostAddress();
+                    ip = (InetAddress) addresses.nextElement();
+                    if (ip != null && ip instanceof Inet4Address)
+                    {
+                        if (!ip.getHostAddress().startsWith("127.0.0.1") && !ip.getHostAddress().startsWith("localhost")){
+                            ipAddress = ip.getHostAddress();
+                        }
                     }
                 }
             }
-        }
-        if (ipAddress.equals("127.0.0.1")){
-            InetAddress addr = InetAddress.getLocalHost();
-            ipAddress = addr.getHostAddress();
-        }
-        if ("127.0.0.1".equals(ipAddress)){
-            log.warn("本机IP的局域网地址获取可能失败（返回默认的127.0.0.1），当前系统环境：{}",osName);
+            if ("127.0.0.1".equals(ipAddress)){
+                InetAddress addr = InetAddress.getLocalHost();
+                ipAddress = addr.getHostAddress();
+            }
+            if ("127.0.0.1".equals(ipAddress)){
+                log.warn("本机IP的局域网地址获取可能失败（返回默认的127.0.0.1），当前系统环境：{}",osName);
+            }
+        } catch (Exception e) {
+            log.error("",e);
         }
         return ipAddress;
     }
@@ -108,26 +116,29 @@ public class HostUtil {
     /**
      * 获取本机所有的IP地址（不包括127.0.0.1等IP地址）
      * @return
-     * @throws UnknownHostException
      */
-    public static List<String> getHostIps() throws UnknownHostException, SocketException {
+    public static List<String> getHostIps() {
         List<String> ipAddress = new ArrayList<>();
-        Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
-        InetAddress ip;
-        while (allNetInterfaces.hasMoreElements())
-        {
-            NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
-            Enumeration addresses = netInterface.getInetAddresses();
-            while (addresses.hasMoreElements())
+        try {
+            Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            InetAddress ip;
+            while (allNetInterfaces.hasMoreElements())
             {
-                ip = (InetAddress) addresses.nextElement();
-                if (ip != null && ip instanceof Inet4Address)
+                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+                Enumeration addresses = netInterface.getInetAddresses();
+                while (addresses.hasMoreElements())
                 {
-                    if (!ip.getHostAddress().startsWith("127.0.0.1") && !ip.getHostAddress().startsWith("localhost")){
-                        ipAddress.add(ip.getHostAddress());
+                    ip = (InetAddress) addresses.nextElement();
+                    if (ip != null && ip instanceof Inet4Address)
+                    {
+                        if (!ip.getHostAddress().startsWith("127.0.0.1") && !ip.getHostAddress().startsWith("localhost")){
+                            ipAddress.add(ip.getHostAddress());
+                        }
                     }
                 }
             }
+        } catch (Exception e) {
+            log.error("",e);
         }
 
         return ipAddress;
